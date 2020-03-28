ab8c611e3c72440364fe61d09f9bf0ea5edfabf8
Eason Li <lijinghua@odc.cmbchina.cn>
2020-03-28 19:48:24 +0800

    Updates

---
+++ precache-manifest.1127610029adab237bb09b67549c6092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f7d72030ce275ceaeaa512003cc311",
+    "url": "/far/index.html"
+  },
+  {
+    "revision": "037a15eabe03a432b9a4",
+    "url": "/far/static/css/main.f74953e5.chunk.css"
+  },
+  {
+    "revision": "52ee150c52c3b222adc0",
+    "url": "/far/static/js/2.0f3266e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/far/static/js/2.0f3266e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037a15eabe03a432b9a4",
+    "url": "/far/static/js/main.5e0a4f73.chunk.js"
+  },
+  {
+    "revision": "47fd5d79045094fbd99d",
+    "url": "/far/static/js/runtime-main.85f1febd.js"
+  }
+]);
