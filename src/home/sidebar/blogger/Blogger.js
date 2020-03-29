71c1b694f5761f91f103687d16e5a9be8e31d303
Eason Li <lijinghua@odc.cmbchina.cn>
2020-03-29 18:37:07 +0800

    Complete blogger card

--- src/home/sidebar/blogger/Blogger.js
+++ src/home/sidebar/blogger/Blogger.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import logo from './myself.jpg';
+import location from './location.svg';
+import github from './github.svg';
+import gitlab from './gitlab.svg';
+import wechat from './we-chat.svg';
+import microBlog from './micro-blog.svg';
 
 function Blogger() {
   return (
-    <header className="card border-0">
+    <header className="card border-0 shadow">
       <figure>
         <img
           src={logo}
@@ -13,12 +18,38 @@ function Blogger() {
         <figcaption className="figure-caption">
           <h5 className="card-title text-center">李景华</h5>
           <h6 class="card-subtitle mb-2 text-muted text-center">
-            软件咨询师/培训师
+            <small class="text-muted">IT咨询师/培训师</small>
           </h6>
           <h6 class="card-subtitle mb-2 text-muted text-center">
-            中国湖北武汉
+            <img src={location} alt="location" />
+            <small class="text-muted">武汉，中国湖北</small>
           </h6>
         </figcaption>
+        <div className="card-body pb-0">
+          <button
+            type="button"
+            class="btn btn-primary btn-sm btn-block rounded-pill">
+            了解我
+          </button>
+          <ul class="list-inline mb-0 mt-2 text-center">
+            <li class="list-inline-item">
+              <a href="https://github.com/EasonAndLily">
+                <img src={github} alt="github"></img>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://tw-baseline-exam.gitlab.io/to-do-list-front-end/">
+                <img src={gitlab} alt="gitlab"></img>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <img src={wechat} alt="github"></img>
+            </li>
+            <li class="list-inline-item">
+              <img src={microBlog} alt="github"></img>
+            </li>
+          </ul>
+        </div>
       </figure>
     </header>
   );
