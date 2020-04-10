60a286175a5e6c00f47358cc714c5274966a6f51
Eason Li <lijinghua@odc.cmbchina.cn>
2020-04-10 22:06:32 +0800

    Add course attrs

--- src/course/Course.js
+++ src/course/Course.js
@@ -4,13 +4,17 @@ import './Course.css';
 function Course() {
   return (
     <section className="col col-sm-9 opacity-9">
-      <div class="jumbotron jumbotron-fluid jumbotron-bg rounded shadow">
-        <div class="container">
-          <h1 class="display-4 text-center text-white">Java SE 基础课程</h1>
-          <p class="lead text-center text-white">
-            This is a modified jumbotron that occupies the entire horizontal
-            space of its parent.
-          </p>
+      <div className="jumbotron jumbotron-fluid jumbotron-bg rounded shadow">
+        <div className="container">
+          <h1 className="display-4 text-center text-white">Java SE 基础课程</h1>
+          <div className="text-center text-white">
+            <ul className="course-attr p-0">
+              <li>难度：入门</li>
+              <li>时长：30分钟</li>
+              <li>人数：1235人</li>
+              <li>作者：李景华</li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
