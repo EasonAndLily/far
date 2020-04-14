58ba590d768d79ea84789b0390d9f42257546fc3
Eason Li <lijinghua@odc.cmbchina.cn>
2020-04-14 22:32:47 +0800

    Add course sections

--- src/course/Course.js
+++ src/course/Course.js
@@ -108,7 +108,7 @@ function Course() {
                 <div className="card-header bg-white border-0 font-weight-bold p-1">
                   {item.title}
                 </div>
-                <img src={item.image}  alt="..." />
+                <img src={item.image} alt="..." />
                 <div className="card-body pt-2">
                   <h6 className="card-title">{item.name}</h6>
                   <p className="card-text">
@@ -121,6 +121,103 @@ function Course() {
                 </div>
               </div>
             ))}
+          </div>
+        </div>
+        <div className="accordion" id="accordionExample">
+          <h5 className="pl-4">课程章节</h5>
+          <div className="card border-0">
+            <div className="card-header p-0 bg-white border-0" id="headingOne">
+              <button
+                className="btn btn-light btn-sm btn-block pl-5 text-left"
+                type="button"
+                data-toggle="collapse"
+                data-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne">
+                第一章：Java基础语法
+              </button>
+            </div>
+
+            <div
+              id="collapseOne"
+              class="collapse show"
+              aria-labelledby="headingOne"
+              data-parent="#accordionExample">
+              <div class="card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life
+                accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                non cupidatat skateboard dolor brunch. Food truck quinoa
+                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
+                put a bird on it squid single-origin coffee nulla assumenda
+                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
+                wes anderson cred nesciunt sapiente ea proident. Ad vegan
+                excepteur butcher vice lomo. Leggings occaecat craft beer
+                farm-to-table, raw denim aesthetic synth nesciunt you probably
+                haven't heard of them accusamus labore sustainable VHS.
+              </div>
+            </div>
+          </div>
+          <div class="card border-0">
+            <div class="card-header p-0 bg-white border-0" id="headingTwo">
+              <button
+                class="btn btn-light btn-sm btn-block pl-5 text-left"
+                type="button"
+                data-toggle="collapse"
+                data-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo">
+                第二章：面向对象编程
+              </button>
+            </div>
+            <div
+              id="collapseTwo"
+              class="collapse"
+              aria-labelledby="headingTwo"
+              data-parent="#accordionExample">
+              <div class="card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life
+                accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                non cupidatat skateboard dolor brunch. Food truck quinoa
+                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
+                put a bird on it squid single-origin coffee nulla assumenda
+                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
+                wes anderson cred nesciunt sapiente ea proident. Ad vegan
+                excepteur butcher vice lomo. Leggings occaecat craft beer
+                farm-to-table, raw denim aesthetic synth nesciunt you probably
+                haven't heard of them accusamus labore sustainable VHS.
+              </div>
+            </div>
+          </div>
+          <div class="card border-0">
+            <div class="card-header p-0 bg-white border-0" id="headingThree">
+              <button
+                class="btn btn-light btn-sm btn-block pl-5 text-left"
+                type="button"
+                data-toggle="collapse"
+                data-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree">
+                第三章：java基础进阶
+              </button>
+            </div>
+            <div
+              id="collapseThree"
+              class="collapse"
+              aria-labelledby="headingThree"
+              data-parent="#accordionExample">
+              <div class="card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life
+                accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                non cupidatat skateboard dolor brunch. Food truck quinoa
+                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
+                put a bird on it squid single-origin coffee nulla assumenda
+                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
+                wes anderson cred nesciunt sapiente ea proident. Ad vegan
+                excepteur butcher vice lomo. Leggings occaecat craft beer
+                farm-to-table, raw denim aesthetic synth nesciunt you probably
+                haven't heard of them accusamus labore sustainable VHS.
+              </div>
+            </div>
           </div>
         </div>
       </div>
