7418c09c17bbb0a5087d3c049693332812d53b8b
Eason Li <lijinghua@odc.cmbchina.cn>
2020-04-25 22:09:19 +0800

    Add experience for training

--- src/about/me/Me.js
+++ src/about/me/Me.js
@@ -16,6 +16,8 @@ import certificate from './certificate.svg';
 import CET from './CET.svg';
 import experience from './experience.svg';
 import company from './company.svg';
+import project from './project.svg';
+import time from './time.svg';
 
 function Me() {
   return (
@@ -151,6 +153,61 @@ function Me() {
                 <li>擅长以段子驱动的教学方式。</li>
               </ul>
             </p>
+          </div>
+        </div>
+        <div className="card shadow rounded mt-3">
+          <h5 className="card-header p-2">
+            <img src={experience} alt="" className="img-mt-4 mr-2"></img>
+            培训工作经验--1.5年
+          </h5>
+          <div class="card-body p-2">
+            <h6 class="card-title mb-1">
+              <img src={project} alt="" className="img-mt-4 mr-2"></img>
+              TW非计算机毕业生培训
+              <img src={time} alt="" className="img-mt-4 mr-1 ml-5"></img>
+              2018/11 ~ 2020/06
+            </h6>
+            <hr className="border border-dark m-0"></hr>
+            <h6 className="mb-0">项目描述：</h6>
+            <p class="card-text mb-1 font-14 text-indent-2">
+              非计培训是《TW
+              零基础IT人才战略》的重要组成部分，在公司规模化扩张的道路上，形成对不同背景的毕业生进行规模化的培养体系，是TW快速发展的后勤保障，为公司差异化的竞争提供能量补给。培训内容为Web基础技能入门，包括前端(HTML/HTML5/CSS/CSS3/JS/ES6)、后端（Restful/Java/MySQL)、软件工程等。本人连续做了两期，第一期为讲师，第二期为PM。
+            </p>
+            <h6 className="mb-0">主要职责：</h6>
+            <ul className="font-14 mb-1">
+              <li>
+                作为非计培训PM，负责非计培训产品的设计、规划、人才梯队的搭建、考核标准的制定等。
+              </li>
+              <li>
+                作为非计培训讲师，负责非计课程的研发、教学内容的设计、授课、作业批改、线上答疑等。
+              </li>
+              <li>
+                负责将非计培训成果的给相关Stakeholder展示，拉通，风险学员的追踪等。
+              </li>
+            </ul>
+            <h6 class="card-title mb-1">
+              <img src={project} alt="" className="img-mt-4 mr-2"></img>
+              OOCL应届毕业生培训
+              <img src={time} alt="" className="img-mt-4 mr-1 ml-5"></img>
+              2019/07 ~ 2019/08
+            </h6>
+            <hr className="border border-dark m-0"></hr>
+            <h6 className="mb-0">项目描述：</h6>
+            <p class="card-text mb-1 font-14 text-indent-2">
+              OOCL计算机专业应届毕业生入职后培训，总共学员80人，分为4个班，本人作为其中一个班的班主任。主要培训课程包括前端(HTML/CSS/JS/ES6/Vue)、后端(Java/Spring
+              Boot/Spring Data
+              JPA/Flyway)、编程思想(Restful/TDD/Tasking/概念图)、敏捷实践等。
+            </p>
+            <h6 className="mb-0">主要职责：</h6>
+            <ul className="font-14 mb-1">
+              <li>
+                作为班主任，负责整个班培训目标的制定、培训体系的搭建、验收标准的制定、流程把控等。
+              </li>
+              <li>负责课程内容开发、教学场景、教学形式的设计和实施。</li>
+              <li>负责教学结果的呈现、客户关系的管理、教学成果的汇报。</li>
+              <li>负责新讲师的培养、讲师能力提升体系的搭建等。</li>
+              <li>负责部分课程的授课、作业批改、答疑等。</li>
+            </ul>
           </div>
         </div>
       </div>
