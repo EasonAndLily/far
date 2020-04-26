57d3f1b8f835bebcb28eefd337a84a88eff539f6
Eason Li <lijinghua@odc.cmbchina.cn>
2020-04-26 21:27:55 +0800

    Set title bold

--- src/about/me/Me.js
+++ src/about/me/Me.js
@@ -29,7 +29,7 @@ function Me() {
           <img src={me} className="card-image img-fluid" alt=""></img>
         </div>
         <div className="card mt-3 shadow">
-          <h5 className="card-header pb-0 pt-1 pl-2 pr-1">
+          <h5 className="card-header pb-0 pt-1 pl-2 pr-1 font-weight-bold">
             <img src={info} alt="" className="img-mt-4"></img>
             基本信息
           </h5>
@@ -52,7 +52,7 @@ function Me() {
           </div>
         </div>
         <div className="card mt-3 shadow">
-          <h5 className="card-header pb-1 pt-1 pl-2 pr-1">
+          <h5 className="card-header pb-1 pt-1 pl-2 pr-1 font-weight-bold">
             <img src={education} alt="" className="img-mt-4 mr-2"></img>
             教育背景
           </h5>
@@ -76,7 +76,7 @@ function Me() {
           </div>
         </div>
         <div className="card mt-3 shadow">
-          <h5 className="card-header pb-1 pt-1 pl-2 pr-1">
+          <h5 className="card-header pb-1 pt-1 pl-2 pr-1 font-weight-bold">
             <img src={experience} alt="" className="img-mt-4 mr-2"></img>
             工作经历--5年
           </h5>
@@ -103,7 +103,7 @@ function Me() {
           </div>
         </div>
         <div className="card mt-3 shadow">
-          <h5 className="card-header pb-1 pt-1 pl-2 pr-1">
+          <h5 className="card-header pb-1 pt-1 pl-2 pr-1 font-weight-bold">
             <img src={feature} alt="" className="img-mt-4 mr-2"></img>
             个人特色
           </h5>
@@ -120,7 +120,7 @@ function Me() {
           </div>
         </div>
         <div className="card mt-3 shadow">
-          <h5 className="card-header pb-1 pt-1 pl-2 pr-1">
+          <h5 className="card-header pb-1 pt-1 pl-2 pr-1 font-weight-bold">
             <img src={evaluation} alt="" className="img-mt-4 mr-2"></img>
             自我评价
           </h5>
@@ -140,12 +140,12 @@ function Me() {
         </h4>
         <hr className="border border-dark m-0"></hr>
         <div className="card shadow rounded mt-3">
-          <h5 className="card-header p-2">
+          <h5 className="card-header p-2 font-weight-bold">
             <img src={skills} alt="" className="img-mt-4 mr-2"></img>
             基本技能
           </h5>
           <div class="card-body p-2">
-            <h6 class="card-title mb-1">
+            <h6 class="card-title mb-1 font-weight-bold">
               <img src={software} alt="" className="img-mt-2 mr-2"></img>
               软件工程基本技能
             </h6>
@@ -165,7 +165,7 @@ function Me() {
                 <li>操作系统：Windows/Linux/Mac</li>
               </ul>
             </p>
-            <h6 class="card-title mb-1">
+            <h6 class="card-title mb-1 font-weight-bold mt-2">
               <img src={training} alt="" className="img-mt-2 mr-2"></img>
               IT培训基本技能
             </h6>
@@ -188,24 +188,24 @@ function Me() {
           </div>
         </div>
         <div className="card shadow rounded mt-3">
-          <h5 className="card-header p-2">
+          <h5 className="card-header p-2 font-weight-bold">
             <img src={experience} alt="" className="img-mt-4 mr-2"></img>
             培训工作经验--1.5年
           </h5>
           <div class="card-body p-2">
-            <h6 class="card-title mb-1">
+            <h6 class="card-title mb-1 font-weight-bold">
               <img src={project} alt="" className="img-mt-4 mr-2"></img>
               TW非计算机毕业生培训
               <img src={time} alt="" className="img-mt-4 mr-1 ml-5"></img>
               2018/11 ~ 2020/06
             </h6>
             <hr className="border border-dark m-0"></hr>
-            <h6 className="mb-0">项目描述：</h6>
+            <h6 className="mb-0 font-weight-bold">项目描述：</h6>
             <p class="card-text mb-1 font-14 text-indent-2">
               非计培训是《TW
               零基础IT人才战略》的重要组成部分，在公司规模化扩张的道路上，形成对不同背景的毕业生进行规模化的培养体系，是TW快速发展的后勤保障，为公司差异化的竞争提供能量补给。培训内容为Web基础技能入门，包括前端(HTML/HTML5/CSS/CSS3/JS/ES6)、后端（Restful/Java/MySQL)、软件工程等。本人连续做了两期，第一期为讲师，第二期为PM。
             </p>
-            <h6 className="mb-0">主要职责：</h6>
+            <h6 className="mb-0 font-weight-bold">主要职责：非计PM/讲师</h6>
             <ul className="font-14 mb-1">
               <li>
                 作为非计培训PM，负责非计培训产品的设计、规划、人才梯队的搭建、考核标准的制定等。
@@ -217,20 +217,20 @@ function Me() {
                 负责将非计培训成果的给相关Stakeholder展示，拉通，风险学员的追踪等。
               </li>
             </ul>
-            <h6 class="card-title mb-1">
+            <h6 class="card-title mb-1 mt-2 font-weight-bold">
               <img src={project} alt="" className="img-mt-4 mr-2"></img>
               OOCL应届毕业生培训
               <img src={time} alt="" className="img-mt-4 mr-1 ml-5"></img>
               2019/07 ~ 2019/08
             </h6>
             <hr className="border border-dark m-0"></hr>
-            <h6 className="mb-0">项目描述：</h6>
+            <h6 className="mb-0 font-weight-bold">项目描述：</h6>
             <p class="card-text mb-1 font-14 text-indent-2">
               OOCL计算机专业应届毕业生入职后培训，总共学员80人，分为4个班，本人作为其中一个班的班主任。主要培训课程包括前端(HTML/CSS/JS/ES6/Vue)、后端(Java/Spring
               Boot/Spring Data
               JPA/Flyway)、编程思想(Restful/TDD/Tasking/概念图)、敏捷实践等。
             </p>
-            <h6 className="mb-0">主要职责：</h6>
+            <h6 className="mb-0 font-weight-bold">主要职责：班主任/讲师</h6>
             <ul className="font-14 mb-1">
               <li>
                 作为班主任，负责整个班培训目标的制定、培训体系的搭建、验收标准的制定、流程把控等。
