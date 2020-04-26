c07957d3646744fe0ff328fe4243b8ef8111e1f9
Eason Li <lijinghua@odc.cmbchina.cn>
2020-04-26 21:33:52 +0800

    Set result page style

--- src/about/About.js
+++ src/about/About.js
@@ -3,7 +3,7 @@ import Me from './me/Me';
 
 function About() {
   return (
-    <section className="col opacity-9 bg-white shadow rounded">
+    <section className="col opacity-9 bg-white shadow rounded mb-3">
       <Me></Me>
     </section>
   );
