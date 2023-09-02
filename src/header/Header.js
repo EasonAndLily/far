654d0f035d25c7d716aadc8c6cd29dfee11ba7c9
Eason <15342349100@163.com>
2023-09-02 23:00:10 +0800

    update header

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './Header.css';
-import logo from './logo.png';
+import logo from '../logo.png';
 import searchBtn from './search-btn.svg';
-import login from './login.svg';
-import signUp from './sign-up.svg';
 import Category from '../sidebar/category/Category.js';
 
 function Header() {
@@ -14,28 +12,20 @@ function Header() {
           <div className="col-2 ml-2">
             <a className="navbar-brand" href="##">
               <img src={logo} alt="logo" className="logo ml-1 mr-2" />
-              <span className="d-none d-sm-inline-block">捷登之旅</span>
+              <span className="d-none d-sm-inline-block logo-title">捷登社区</span>
             </a>
           </div>
 
           <div className="col-8 col-sm-10 row justify-content-end pt-1">
-            <div className="col-sm-10 row justify-content-end mr-2">
+            <div className="row justify-content-end mr-2">
               <input
-                className="search-input mt-2 w-75"
+                className="search-input mt-2"
                 type="search"
                 placeholder="输入内容检索关键字"
               />
               <button className="btn btn-sm search-btn" type="submit">
                 <img src={searchBtn} alt="Search"></img>
               </button>
-            </div>
-            <div className="col-sm-2 pr-0 d-none d-sm-block pt-2 text-right">
-              <a className="m-2" href="##">
-                <img src={signUp} alt="sign up"></img>
-              </a>
-              <a className="m-2" href="##">
-                <img src={login} alt="login"></img>
-              </a>
             </div>
           </div>
 
