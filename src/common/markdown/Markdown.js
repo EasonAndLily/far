1dbe068a18d4aa70b47ef068d26888cd610c52a7
Eason <15342349100@163.com>
2023-09-09 23:56:53 +0800

    调整code的样式

--- src/common/markdown/Markdown.js
+++ src/common/markdown/Markdown.js
@@ -1,34 +1,42 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import CodeBlock from './CodeBlock.js';
+import remarkGfm from 'remark-gfm'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { coldarkCold } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 function Markdown({ source }) {
   return (
-    // <ReactMarkdown
-    //   source={source}
-    //   escapeHtml={false}
-    //   renderers={{ code: CodeBlock }}></ReactMarkdown>
     <ReactMarkdown components={{
+      h1: ({ node, ...props }) => <div>
+        <h1 {...props} />
+        <hr></hr>
+      </div>,
+      h2: ({ node, ...props }) => <div>
+        <h2 {...props} />
+        <hr></hr>
+      </div>,
       img(props) {
         return <img {...props} style={{ maxWidth: '100%' }}></img>
       },
-      code: ({ children = [], className, ...props }) => {
-        const match = /language-(\w+)/.exec(className || '')
-        return (<SyntaxHighlighter
-                language={match?.[1]}
-                showLineNumbers={true}
-                style={coldarkCold}
-                PreTag='div'
-                className='syntax-hight-wrapper'
-                {...props}
-              >
-                {children}
-              </SyntaxHighlighter>)
-    }
-    }} children={source}
-      renderers={{ code: CodeBlock }}></ReactMarkdown>
+      code({ node, inline, className, children, ...props }) {
+        const match = /language-(\w+)/.exec(className || "");
+
+        return !inline && match ? (
+          <SyntaxHighlighter
+            style={coldarkCold}
+            PreTag="div"
+            language={match[1]}
+            children={String(children).replace(/\n$/, "")}
+            showLineNumbers={true}
+            {...props}
+          />
+        ) : (
+          <code className={className ? className : ""} {...props}>
+            {children}
+          </code>
+        );
+      }
+    }} children={source} remarkPlugins={[remarkGfm]}></ReactMarkdown>
   );
 }
 
